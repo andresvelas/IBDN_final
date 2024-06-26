13cdf01bd4834d6a738d1a8d2e709802ed4dff91
andresvelas <andres.velasco@alumnos.upm.es>
2024-06-27 01:19:54 +0200

    Dockercompose casi terminado: pyflask y spark empujon

--- scripts/kafka/producer.sh
+++
@@ -1,3 +0,0 @@
-
-
-docker exec -ti $(docker ps -f "name=kafka" -q) bash -c "opt/bitnami/kafka/bin/kafka-console-producer.sh --topic flight_delay_classification_request --bootstrap-server kafka:9092 < $1 \ $2"
